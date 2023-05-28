f1010882a229380f419810c816f93782a4b69557
alizeinoddini <134832865+alizeinoddini@users.noreply.github.com>
2023-05-28 11:19:51 +0330

    Add files via upload

---
+++ js/project.js
@@ -0,0 +1,39 @@
+
+// $(document).ready(function () {
+// 	'use strict';
+
+// 	if ($.fn.countdown) {
+	
+// 		$('.daily-deal-countdown').each(function () {
+// 			var $this = $(this),
+// 				untilDate = $this.data('until'),
+// 				compact = $this.data('compact');
+
+// 			$this.countdown({
+// 				until: untilDate, 
+// 				format: 'HMS',
+// 				padZeroes: true,
+// 				labels: ['سال', 'ماه', 'هفته', 'روز', 'ساعت', 'دقیقه', 'ثانیه'],
+// 				labels1: ['سال', 'ماه', 'هفته', 'روز', 'ساعت', 'دقیقه', 'ثانیه']
+// 			});
+// 		});
+
+	
+// 		$('.offer-countdown').each(function () {
+// 			var $this = $(this),
+// 				untilDate = $this.data('until'),
+// 				compact = $this.data('compact');
+
+// 			$this.countdown({
+// 				until: untilDate,
+// 				format: 'DHMS',
+// 				padZeroes: true,
+// 				labels: ['سال', 'ماه', 'هفته', 'روز', 'ساعت', 'دقیقه', 'ثانیه'],
+// 				labels1: ['سال', 'ماه', 'هفته', 'روز', 'ساعت', 'دقیقه', 'ثانیه']
+// 			});
+// 		});
+
+		
+// 	}
+// });
+
