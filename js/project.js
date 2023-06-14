f26d19f848cab1751ddd0831536d2a37903a0de5
alizeinoddini <ali.zeinoddini1379@gmail>
2023-06-14 17:31:43 +0330

    Add existing project files prior to the push to GitHub.

---
+++ js/project.js
@@ -0,0 +1,12 @@
+
+// $(document).ready(function () {
+// 	'use strict';
+
+$('.sidebar-filter-clear').on('click', function (e) {
+    $('.sidebar-shop').find('input').prop('checked', false);
+
+    e.preventDefault();
+});
+// 	}
+// });
+
